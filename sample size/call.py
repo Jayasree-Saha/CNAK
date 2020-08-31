8de410242245796de69cb982c540a22e366ea012
Jayasree Saha <jayasree.saha.edu@gmail.com>
2020-09-01 00:28:36 +0530

    Add files via upload

---
+++ sample size/call.py
@@ -0,0 +1,6 @@
+import Sample_Size_Determination as SSD
+
+path="/home/jayasree/jayasree/2018/first_work/newdatasetCreation/simulations/sim10"
+fileName="sim10_12.csv"
+dim=2
+SSD.main(path, fileName, dim)
